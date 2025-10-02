e7b679b48b3f906360b02d074f73d64e5c6e7c20
RAMÓN ANGEL AGUILAR RODRÍGUEZ <RamonAngel.Aguilar@alu.uclm.es>
2025-10-02 18:16:08 +0200

    Creada la clase ResenyaBocadillo con los atributos correspondientes exceptuando aquellos que forman las relaciones de la BBDD #17

---
+++ src/AppForSEII2526.API/Models/ResenyaBocadillo.cs
@@ -0,0 +1,14 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [PrimaryKey(nameof(BocadilloId),nameof(ResenyaId))]
+    public class ResenyaBocadillo
+    {
+        public int BocadilloId { get; set; }
+        public int ResenyaId { get; set; }
+
+        [Range(1, 10, ErrorMessage = "La puntuación del bocadillo tiene un rango del 1 al 10")]
+        [Display(Name = "Puntuación del bocadillo")]
+        public int Puntuacion { get; set; }
+
+    }
+}
