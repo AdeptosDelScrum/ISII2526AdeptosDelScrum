5270494453f472a214b1a438b057a0398e0c19c5
Ari <arirushwars@gmail.com>
2025-10-02 16:41:20 +0200

    Relaciones de TipoPan #16

--- src/AppForSEII2526.API/Models/TipoPan.cs
+++ src/AppForSEII2526.API/Models/TipoPan.cs
@@ -10,7 +10,14 @@ namespace AppForSEII2526.API.Models
 
         [Required, StringLength(30, ErrorMessage = "El nombre no puede tener más de 30 caracteres.")]
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
-        public string Nombre { get; set; }
+        public String Nombre { get; set; }
+
+        /*
+         
+        [Required]
+        public List<Bocadillo> Bocadillos { get; set; }
+
+         */
 
         public override bool Equals(object? obj)
         {
