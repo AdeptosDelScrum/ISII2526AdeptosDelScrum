2e8188150ca8c9fd7c08e65084f717b8721acc32
Ariadna Rodríguez <ariadna.rodriguez2@alu.uclm.es>
2025-10-03 11:19:18 +0200

    13 crear clase bocadillo (#28)
    * Creada clase "Bocadillo" #13
    
    * Relaciones de Bocadillo dentro del caso de uso "Pedir Bocadillo" #13
    
    Elementos de trabajo relacionados: #2; #13
    
    ---------
    
    Co-authored-by: ARIADNA RODRIGUEZ FERNANDEZ <70413458+Ari-7@users.noreply.github.com>
    Co-authored-by: Ari <arirushwars@gmail.com>

---
+++ src/AppForSEII2526.API/Models/Bocadillo.cs
@@ -0,0 +1,51 @@
+﻿using DataType = System.ComponentModel.DataAnnotations.DataType;
+
+namespace AppForSEII2526.API.Models
+{
+    public class Bocadillo
+    {
+
+        public Bocadillo() { }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(30, ErrorMessage = "El nombre no puede tener más de 30 caracteres.")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        public String Nombre { get; set; }
+
+        [Required, DataType(DataType.Currency)]
+        [Display(Name = "Total")]
+        public float PVP {  get; set; }
+
+        [Required]
+        public int Stock {  get; set; }
+
+        [Required]
+        public float Tamanyo {  get; set; }
+
+        /*
+        [Required]
+        public TipoPan TipoPan {  get; set; }
+
+        [Required]
+        public List<CompraBocadillo> ComprasDelBocadillo {  get; set; }
+         
+         */
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Bocadillo bocadillo &&
+                   Id == bocadillo.Id &&
+                   Nombre == bocadillo.Nombre &&
+                   PVP == bocadillo.PVP &&
+                   Stock == bocadillo.Stock &&
+                   Tamanyo == bocadillo.Tamanyo;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Nombre, PVP, Stock, Tamanyo);
+        }
+    }
+}
