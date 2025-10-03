547e2b387162037dbb1f84dad46a04a501408f77
Ariadna Rodríguez <ariadna.rodriguez2@alu.uclm.es>
2025-10-03 11:22:06 +0200

    16 crear clase tipopan (#30)
    * Creada la clase "TipoPan" #16
    
    * Cambio en StringLength de TipoPan.Nombre #16 6
    
    * Relaciones de TipoPan #16
    
    ---------
    
    Co-authored-by: ARIADNA RODRIGUEZ FERNANDEZ <70413458+Ari-7@users.noreply.github.com>
    Co-authored-by: Ari <arirushwars@gmail.com>

---
+++ src/AppForSEII2526.API/Models/TipoPan.cs
@@ -0,0 +1,34 @@
+﻿
+namespace AppForSEII2526.API.Models
+{
+    public class TipoPan
+    {
+        public TipoPan() { }
+
+        [Key]
+        public int PanId { get; set; }
+
+        [Required, StringLength(30, ErrorMessage = "El nombre no puede tener más de 30 caracteres.")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        public String Nombre { get; set; }
+
+        /*
+         
+        [Required]
+        public List<Bocadillo> Bocadillos { get; set; }
+
+         */
+
+        public override bool Equals(object? obj)
+        {
+            return obj is TipoPan pan &&
+                   PanId == pan.PanId &&
+                   Nombre == pan.Nombre;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(PanId, Nombre);
+        }
+    }
+}
