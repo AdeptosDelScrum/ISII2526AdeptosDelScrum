26e5bf5c490f8b38a2ba27fdf45fc22ae07e91f7
RamonUCLM <ramonangel.aguilar@alu.uclm.es>
2025-10-03 09:41:08 +0200

    15 implementación de la clase resenya (#25)
    * Creada clase Resenya con los atributos mencionados en el CU exceptuando las relaciones, modificado ApplicationDBContext para añadir la clase al modelo #15
    
    * Agregado un enum para la valoración en la clase resenya, eliminados los métodos equals y getHashCode #15

---
+++ src/AppForSEII2526.API/Models/Resenya.cs
@@ -0,0 +1,40 @@
+﻿using DataType = System.ComponentModel.DataAnnotations.DataType;
+
+namespace AppForSEII2526.API.Models
+{
+    public class Resenya
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required,StringLength(150,ErrorMessage = "La descripción puede tener como máximo 150 caracteres", MinimumLength = 10)]
+        [Display(Name = "Descripción")]
+        public string descripcion { get; set; }
+
+        [Required]
+        [DataType(DataType.Date), Display(Name = "Fecha de publicación")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime fechaPublicacion { get; set; }
+
+        [Required,StringLength(20, ErrorMessage = "El nombre de usuario debe tener máximo 20 caracteres")]
+        [Display(Name = "Nombre de usuario")]
+        public string? nombreUsuario {  get; set; }
+
+        [Required,StringLength(25, ErrorMessage = "El título debe tener máximo 25 caracteres", MinimumLength = 1)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Display(Name = "Título")]
+        public string titulo {  get; set; }
+
+        [Required]
+        [Display(Name = "Valoración general")]
+        public rate valoracion { get; set; }
+
+        public enum rate { 
+            Una,
+            Dos,
+            Tres,
+            Cuatro,
+            Cinco
+        }
+    }
+}
