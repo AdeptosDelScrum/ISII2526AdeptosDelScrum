3b98986658f042ab1778650f8d78881ca9d14a18
Ariadna Rodríguez <ariadna.rodriguez2@alu.uclm.es>
2025-10-05 02:40:38 +0200

    Hechos los cambios en las clases relacionadas con el método de pago y solucionada la discrepancia de la clase "Compra" con el CU propuesto. #37 (#38)

--- src/AppForSEII2526.API/Models/Tarjeta.cs
+++ src/AppForSEII2526.API/Models/Tarjeta.cs
@@ -1,8 +1,6 @@
 ﻿namespace AppForSEII2526.API.Models
 {
-    public sealed class Tarjeta: MetodoPago
+    public class Tarjeta: MetodoPago
     {
-        public override int Id { get; protected set; }
-        public override string Metodo => "Tarjeta";
     }
 }
