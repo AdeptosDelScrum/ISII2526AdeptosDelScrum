c9559fed905d14b8f0c54f798e2c8d9da651bb4f
carlos122211 <carlos.moreno35@alu.uclm.es>
2025-10-05 21:08:30 +0200

    Update ApplicationDbContext.cs
    he añadido lo necesario para las relaciones de atributos

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -27,6 +27,37 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     protected override void OnModelCreating(ModelBuilder
 builder)
     {
+       
+modelBuilder.Entity<BonoBocadillo>()
+    .HasOne(b => b.TipoBocadillo)
+    .WithMany(t => t.Bonos)
+    .HasForeignKey(b => b.IdTipo)
+    .OnDelete(DeleteBehavior.Restrict);
+
+
+modelBuilder.Entity<BonosComprados>(b =>
+{
+    b.HasKey(x => new { x.CompraId, x.BonoId });
+    b.Property(x => x.PrecioBono).HasColumnType("decimal(10,2)");
+
+    b.HasOne(x => x.Bono)
+     .WithMany(bono => bono.BonosComprados)
+     .HasForeignKey(x => x.BonoId)
+     .OnDelete(DeleteBehavior.Restrict);
+
+    b.HasOne(x => x.Compra)
+     .WithMany(compra => compra.BonosComprados)
+     .HasForeignKey(x => x.CompraId)
+     .OnDelete(DeleteBehavior.Restrict);
+});
+
+
+modelBuilder.Entity<CompraBono>()
+    .HasOne(c => c.MetodoPago)
+    .WithMany() // o .WithMany(mp => mp.Compras) si tu entidad tiene coleccion
+    .HasForeignKey(c => c.MetodoPagoId)
+    .OnDelete(DeleteBehavior.Restrict);
+
         base.OnModelCreating(builder);
 
         builder.Entity<Tamanyo>()
