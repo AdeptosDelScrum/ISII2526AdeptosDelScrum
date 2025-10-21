df8be7309b76191c8d1ecb2e24a619a5c206d964
RAMÓN ANGEL AGUILAR RODRÍGUEZ <RamonAngel.Aguilar@alu.uclm.es>
2025-10-21 10:27:18 +0200

    Creación del DTO de bocadillos para satisfacer lo necesario para los CU Pedir Bocadillo y Crear Resenya #73

---
+++ src/AppForSEII2526.API/DTOs/BocadilloDTO.cs
@@ -0,0 +1,20 @@
+﻿using DataType = System.ComponentModel.DataAnnotations.DataType;
+namespace AppForSEII2526.API.DTOs
+{
+    public class BocadilloDTO
+    {
+        public int Id { get; set; }
+
+        [StringLength(30, ErrorMessage = "El nombre no puede tener más de 30 caracteres.")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        public string Name { get; set; }
+
+        public enum tamanyo { Normal, Pequenyo }
+
+        public TipoPan TipoPan { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Display(Name = "Total")]
+        public float PVP { get; set; }
+    }
+}
