6b1bef547a8e4e339a42c252d308ca862a8471c9
RAMÓN ANGEL AGUILAR RODRÍGUEZ <RamonAngel.Aguilar@alu.uclm.es>
2025-10-27 18:06:46 +0100

    Añadido un constructor a BocadilloDTO, creado el controlador de Bocadillos #75

---
+++ src/AppForSEII2526.API/Controllers/BocadillosController.cs
@@ -0,0 +1,37 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BocadillosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<BocadillosController> _logger;
+
+        public BocadillosController(ApplicationDbContext context, ILogger<BocadillosController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(BocadilloDTO), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetBocadillosParaResenyar(string? nombre, float? PVP)
+        {
+            if (PVP < 0)
+            {
+
+            }
+            var bocadillos = await _context.Bocadillo
+                .Where(b=>(b.Nombre.Contains(nombre) && b.PVP.Equals(PVP)))
+                .Select(b=>new BocadilloDTO(b.Id,b.Nombre,b.TipoPan.Nombre,b.PVP))
+                .ToListAsync();
+            return Ok(bocadillos);
+        }
+        
+    }
+}
--- src/AppForSEII2526.API/DTOs/BocadilloDTO.cs
+++ src/AppForSEII2526.API/DTOs/BocadilloDTO.cs
@@ -3,6 +3,14 @@ namespace AppForSEII2526.API.DTOs
 {
     public class BocadilloDTO
     {
+        public BocadilloDTO(int id, string name, string tipoPan, float pVP)
+        {
+            Id = id;
+            Name = name;
+            TipoPan = tipoPan;
+            PVP = pVP;
+        }
+
         public int Id { get; set; }
 
         [StringLength(30, ErrorMessage = "El nombre no puede tener más de 30 caracteres.")]
@@ -16,5 +24,14 @@ namespace AppForSEII2526.API.DTOs
         [DataType(DataType.Currency)]
         [Display(Name = "Total")]
         public float PVP { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is BocadilloDTO dTO &&
+                   Id == dTO.Id &&
+                   Name == dTO.Name &&
+                   TipoPan == dTO.TipoPan &&
+                   PVP == dTO.PVP;
+        }
     }
 }
