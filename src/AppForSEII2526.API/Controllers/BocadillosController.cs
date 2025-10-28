5d33bdc63e783b039561a56afcb86c23efc362db
Ariadna Rodríguez <ariadna.rodriguez2@alu.uclm.es>
2025-10-28 15:57:35 +0100

    Creada la parte del Get Select para compra de bocadillos. #86 (#96)

---
+++ src/AppForSEII2526.API/Controllers/BocadillosController.cs
@@ -0,0 +1,37 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BocadillosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<BocadillosController> _logger;
+
+        public BocadillosController(ApplicationDbContext context, ILogger<BocadillosController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof (IList<Bocadillo>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetBocadillosParaCompra(Tamanyo? TamanyoBocadillo, string?TipoPanBocadillo)
+        {
+            IList<Bocadillo> bocadillos = (IList<Bocadillo>)await _context.Bocadillo
+                .Include(b => b.ComprasDelBocadillo)
+                .Where(b =>
+                (TipoPanBocadillo == null || b.TipoPan.Nombre.Contains(TipoPanBocadillo)) &&
+                TamanyoBocadillo == null || b.TamanyoBocadillo.Equals(TamanyoBocadillo))
+                .OrderBy(b=> b.Nombre)
+                .Select(b=>new BocadilloDTO(b.Nombre, b.TamanyoBocadillo,b.TipoPan.Nombre, b.PVP))
+                .ToListAsync();
+            return Ok(bocadillos);
+        }
+
+    }
+}
--- src/AppForSEII2526.API/DTOs/BocadilloDTO.cs
+++ src/AppForSEII2526.API/DTOs/BocadilloDTO.cs
@@ -1,20 +1,30 @@
-﻿using DataType = System.ComponentModel.DataAnnotations.DataType;
+﻿using Humanizer.DateTimeHumanizeStrategy;
+using System.Drawing;
+using DataType = System.ComponentModel.DataAnnotations.DataType;
 namespace AppForSEII2526.API.DTOs
 {
     public class BocadilloDTO
     {
+        public BocadilloDTO(string nombre, Tamanyo tamanyo,String tipopan, float precio) { 
+            Name = nombre;
+            TamanyoBocadillo = tamanyo;
+            TipoPan = tipopan;
+            PVP = precio;
+        
+        }
         public int Id { get; set; }
 
         [StringLength(30, ErrorMessage = "El nombre no puede tener más de 30 caracteres.")]
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         public string Name { get; set; }
 
-        public enum tamanyo { Normal, Pequenyo }
-
         public String TipoPan { get; set; }
 
         [DataType(DataType.Currency)]
         [Display(Name = "Total")]
         public float PVP { get; set; }
+
+        public Tamanyo TamanyoBocadillo { get; set; }
+
     }
 }
