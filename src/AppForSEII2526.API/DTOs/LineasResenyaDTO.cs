bb8c9a16d45f7e5dbfd32bcf262c621a22f35068
RAMÓN ANGEL AGUILAR RODRÍGUEZ <RamonAngel.Aguilar@alu.uclm.es>
2025-10-29 15:17:51 +0100

    Creado el DTO de resenya para le método POST, creado el DTO de las lineas de resenya para el DTO de resenya #78

---
+++ src/AppForSEII2526.API/DTOs/LineasResenyaDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class LineasResenyaDTO
+    {
+        public BocadilloDTO bocadillo { get; set; }
+        public string Puntuacion { get; set; }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/ResenyaDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class ResenyaDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Rate { get; set; }
+        public IList<LineasResenyaDTO> Lineas { get; set; }
+
+    }
+}
