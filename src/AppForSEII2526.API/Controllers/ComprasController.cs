38a6f414c450a9a488d0b3d81195a89e2f104f70
Ariadna Rodríguez <ariadna.rodriguez2@alu.uclm.es>
2025-11-05 04:39:40 +0100

    mitad del post hecho

--- src/AppForSEII2526.API/Controllers/ComprasController.cs
+++ src/AppForSEII2526.API/Controllers/ComprasController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using AppForSEII2526.API.DTOs.CompraDTOs;
+using Microsoft.IdentityModel.Tokens;
 namespace AppForSEII2526.API.Controllers
 {
     [Route("api/[controller]")]
@@ -52,6 +53,111 @@ namespace AppForSEII2526.API.Controllers
 
 
             return Ok(rental);
+        }
+        [HttpPost]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(CompraBocadilloDetailDTO), (int)HttpStatusCode.Created)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.Conflict)]
+        public async Task<ActionResult> CrearCompra(CompraBocadilloForCreateDTO compraForCreate)
+        {
+            if (compraForCreate.NombreCliente.IsNullOrEmpty())
+                ModelState.AddModelError("NombreCliente", "El cliente debe ingrsar su nombre");
+
+            if (compraForCreate.Apellido1_cliente.IsNullOrEmpty())
+                ModelState.AddModelError("Apellido1Cliente", "El cliente debe ingrsar su primer apellido");
+
+            if (compraForCreate.MetodoPago.Id.Equals(0))
+                ModelState.AddModelError("MetodoPago", "El cliente debe escoger un método de pago");
+
+            if (compraForCreate.BocadillosComprados.Count == 0)
+                ModelState.AddModelError("BocadillosComprados", "Error! Debes incluir como mínimo un bocadillo para continuar");
+
+            
+            var user = _context.ApplicationUser.FirstOrDefault(au => au.NombreCliente == compraForCreate.NombreCliente);
+            if (user == null)
+                ModelState.AddModelError("CompraApplicationUser", "El usuario no está registrado");
+
+            if (ModelState.ErrorCount > 0)
+                return BadRequest(new ValidationProblemDetails(ModelState));
+
+
+            var bocadillos = compraForCreate.BocadillosComprados.Select(ci => ci.Nombre).ToList<string>();
+
+            var movies = _context.Bocadillo.Include(m => m.ComprasDelBocadillo)
+                .ThenInclude(ci => ci.Compra)
+                .Where(b => bocadillos.Contains(b.Nombre))
+
+                
+                .Select(b => new {
+                    b.Id,
+                    b.Nombre,
+                    b.ComprasDelBocadillo,
+                    b.PVP,
+                    //we count the number of rentalItems that are within the rental period
+                    NumberOfRentedItems = b.ComprasDelBocadillo.Count(ci => ci.Compra.BocadillosComprados <= compraForCreate.BocadillosComprados
+                            && ci.Compra.FechaCompra >= compraForCreate.)
+                })
+                .ToList();
+
+
+            Compra compra = new Compra(compraForCreate.NombreCliente, compraForCreate.Apellido1_cliente, compraForCreate.Apellido2_cliente,
+                  DateTime.Now,
+                compraForCreate.MetodoPago,
+                rentalForCreate.RentalDateFrom, rentalForCreate.RentalDateTo, new List<RentalItem>());
+
+
+            compra.TotalPrice = 0;
+            var numDays = (rental.RentalDateTo - rental.RentalDateFrom).TotalDays;
+
+
+            foreach (var item in rentalForCreate.RentalItems)
+            {
+                var movie = movies.FirstOrDefault(m => m.Title == item.Title);
+                //we must check that there is enough quantity to be rented in the database
+                if ((movie == null) || (movie.NumberOfRentedItems >= movie.QuantityForRenting))
+                {
+                    ModelState.AddModelError("RentalItems", $"Error! Movie titled '{item.Title}' is not available for being rented from {rentalForCreate.RentalDateFrom.ToShortDateString()} to {rentalForCreate.RentalDateTo.ToShortDateString()}");
+                }
+                else
+                {
+                    // rental does not exist in the database yet and does not have a valid Id, so we must relate rentalitem to the object rental
+                    rental.RentalItems.Add(new RentalItem(movie.Id, rental, movie.PriceForRenting, item.Description));
+                    item.PriceForRenting = movie.PriceForRenting;
+                }
+            }
+            rental.TotalPrice = rental.RentalItems.Sum(ri => ri.PriceForRenting * numDays);
+
+
+            //if there is any problem because of the available quantity of movies or because the movie does not exist
+            if (ModelState.ErrorCount > 0)
+            {
+                return BadRequest(new ValidationProblemDetails(ModelState));
+            }
+
+            _context.Add(rental);
+
+            try
+            {
+                //we store in the database both rental and its rentalitems
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                ModelState.AddModelError("Rental", $"Error! There was an error while saving your rental, plese, try again later");
+                return Conflict("Error" + ex.Message);
+
+            }
+
+            //it returns rentalDetail
+            var rentalDetail = new RentalDetailDTO(rental.Id, rental.RentalDate,
+                rental.CustomerUserName, rental.CustomerNameSurname,
+                rental.DeliveryAddress, rentalForCreate.PaymentMethod,
+                rental.RentalDateFrom, rental.RentalDateTo,
+                rentalForCreate.RentalItems);
+
+            return CreatedAtAction("GetRental", new { id = rental.Id }, rentalDetail);
         }
     }
 }
---
+++ src/AppForSEII2526.API/DTOs/CompraDTOs/CompraBocadilloDetailDTO.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.DTOs.CompraDTOs
+{
+
+    public class CompraBocadilloDetailDTO : CompraBocadilloForCreateDTO
+    {
+        public CompraBocadilloDetailDTO(int id, DateTime fecha, float precio, string nombre_cliente, string apellido1_cliente, string? apellido2_cliente, MetodoPago metodoPago, IList<CompraBocadilloItemDTO> bocadillosComprados)
+         : base(nombre_cliente,
+                   apellido1_cliente,
+                   apellido2_cliente,
+                   metodoPago,
+                   bocadillosComprados)
+        {
+            Id = id;
+            Fecha = fecha;
+            Precio = precio;
+
+        }
+
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public float Precio { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CompraBocadilloDetailDTO dTO &&
+                   base.Equals(obj) &&
+                   Precio == dTO.Precio &&
+                   Id == dTO.Id &&
+                   Fecha == dTO.Fecha;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), Id, Fecha, Precio);
+        }
+    }
+}
