16acfee49dadda2c2b5b7a761dcfbc2f17455b06
Ariadna Rodríguez <ariadna.rodriguez2@alu.uclm.es>
2025-11-05 04:09:30 +0100

    adicion de controlador de compras

---
+++ src/AppForSEII2526.API/Controllers/ComprasController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AppForSEII2526.API.DTOs.CompraDTOs;
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComprasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<ComprasController> _logger;
+
+        public ComprasController(ApplicationDbContext context, ILogger<ComprasController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(CompraBocadilloForCreateDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetCompra(int id)
+        {
+            if (_context.Compra == null)
+            {
+                _logger.LogError("Error: La tabla Compra no existe");
+                return NotFound();
+            }
+
+            var rental = await _context.Compra
+             .Where(c => c.CompraId == id)
+                 .Include(c => c.BocadillosComprados)
+                    .ThenInclude(ci => ci.Bocadillo)
+                        .ThenInclude(c => c.TipoPan)
+             .Select(c => new CompraBocadilloDetailDTO(c.CompraId, c.FechaCompra, c.PrecioTotal,
+                                                        c.NombreCliente, c.Apellido1_Cliente,
+                                                        c.Apellido2_Cliente, c.MetodoPago,
+                                                        c.BocadillosComprados
+                                                        .Select(ci => new CompraBocadilloItemDTO(ci.Bocadillo.Nombre,
+                                                                                                ci.Precio, ci.TipoPan,
+                                                                                                ci.Cantidad)).ToList<CompraBocadilloItemDTO>()
+                                                        ))
+             .FirstOrDefaultAsync();
+
+
+            if (rental == null)
+            {
+                _logger.LogError($"Error: La compra con Id {id} no existe");
+                return NotFound();
+            }
+
+
+            return Ok(rental);
+        }
+    }
+}
