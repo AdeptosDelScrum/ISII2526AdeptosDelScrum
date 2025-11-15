66ebbecffbeba901963cdf33843f4ed6d4d4d7e4
Ariadna Rodríguez <ariadna.rodriguez2@alu.uclm.es>
2025-11-15 18:12:50 +0100

    87 creación de método post (#134)
    * adicion de controlador de compras
    
    * mitad del post hecho
    
    * adiciones necesarias para DTOs y post
    
    * Post terminado
    
    * Errores por merge solucionados
    
    * Post hecho

--- src/AppForSEII2526.API/DTOs/CompraDTOs/CompraBocadilloForCreateDTO.cs
+++ src/AppForSEII2526.API/DTOs/CompraDTOs/CompraBocadilloForCreateDTO.cs
@@ -8,25 +8,25 @@ namespace AppForSEII2526.API.DTOs.CompraDTOs
             NombreCliente = nombre_cliente ?? throw new ArgumentNullException(nameof(nombre_cliente)); ;
             Apellido1_cliente = apellido1_cliente ?? throw new ArgumentNullException(nameof(apellido1_cliente)); ;
             Apellido2_cliente = apellido2_cliente;
-            MetodoPago = metodoPago ?? throw new ArgumentNullException(nameof(metodoPago)); ;
-            BocadillosComprados = bocadillosComprados ?? throw new ArgumentNullException(nameof(bocadillosComprados)); 
-        
+            MetodoPago = metodoPago ?? throw new ArgumentNullException(nameof(metodoPago));
+            BocadillosComprados = bocadillosComprados ?? throw new ArgumentNullException(nameof(bocadillosComprados)); ;
         }
-        [Required]
+        
         public string NombreCliente { get; set; }
-        [Required]
+        
         public string Apellido1_cliente { get; set; }
         public string? Apellido2_cliente { get; set; }
         [Required]
         public MetodoPago MetodoPago { get; set; }
         public IList<CompraBocadilloItemDTO> BocadillosComprados { get; set; }
 
+
         public override bool Equals(object? obj)
         {
             return obj is CompraBocadilloForCreateDTO dTO &&
                    NombreCliente == dTO.NombreCliente &&
                    Apellido1_cliente == dTO.Apellido1_cliente &&
-                   Apellido2_cliente == dTO.Apellido2_cliente &&
+                   Apellido2_cliente == dTO.Apellido2_cliente && 
                    MetodoPago == dTO.MetodoPago &&
                    EqualityComparer<IList<CompraBocadilloItemDTO>>.Default.Equals(BocadillosComprados, dTO.BocadillosComprados);
         }
