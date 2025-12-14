bb4f8858741d2b9e7f70a250eef366947c64bfb0
RAMÓN ANGEL AGUILAR RODRÍGUEZ <RamonAngel.Aguilar@alu.uclm.es>
2025-12-14 20:39:38 +0100

    Creado el UIT del caso de uso Crear resenya y el PO de SelectBocadillosParaResenyar #178

---
+++ test/AppForSEII2526.UIT/CrearResenya/CUCrearResenya_UIT.cs
@@ -0,0 +1,52 @@
+﻿using AppForMovies.UIT.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CrearResenya
+{
+    public class CUCrearResenya_UIT : UC_UIT
+    {
+        private SelectBocadillosParaResenyarPO selectBocadillosParaResenyar;
+        private const string bocadilloNombre1 = "Politecnico";
+        private const string bocadilloPVP1 = "10000";
+        private const string tipoPan1 = "sandwitch";
+        private const string tamanyo1 = "Pequenyo";
+        private const string bocadilloNombre2 = "Focata";
+        private const string bocadilloPVP2 = "10";
+        private const string tipoPan2 = "sandwitch";
+        private const string tamanyo2 = "Normal";
+
+
+
+        public CUCrearResenya_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        private void InitialStepsForResenyaBocadillo()
+        {
+            //we wait for the option of the menu to be visible
+            selectBocadillosParaResenyar.WaitForBeingVisible(By.Id("hacerResenya"));
+            //we click on the menu
+            _driver.FindElement(By.Id("hacerResenya")).Click();
+        }
+
+        [Theory]
+        [InlineData(bocadilloNombre1, tipoPan1, bocadilloPVP1, tamanyo1, "Pol", "")]
+        [InlineData(bocadilloNombre2, tipoPan2, bocadilloPVP2, tamanyo2, "", "10")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC2_AF1_UC2_4_5_6_filtering(string name, string tipopan, string pvp, string tamanyo,
+            string filtroName, string filtroPVP)
+        {
+            //Arrange
+            InitialStepsForResenyaBocadillo();
+            var expectedBocadillos = new List<string[]> { new string[] { name, tipopan, pvp, tamanyo }, };
+
+            //Act
+            selectBocadillosParaResenyar.SearchBocadillos(filtroName, filtroPVP);
+
+        }
+    }
+}
---
+++ test/AppForSEII2526.UIT/CrearResenya/SelectBocadillosParaResenyarPO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CrearResenya
+{
+    internal class SelectBocadillosParaResenyarPO : PageObject
+    {
+        private By inputNombre = By.Id("inputNombre");
+        private By inputpvp = By.Id("inputPVP");
+        private By buscar = By.Id("searchBocadillo");
+        private By tableOfBocadillosBy = By.Id("TableOfBocadillos");
+        By errorShownBy = By.Id("ErrorsShown");
+        protected SelectBocadillosParaResenyarPO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+        public void SearchBocadillos(string nombre, string? pvp)
+        {
+            //wait for the webelement to be clickable
+            WaitForBeingClickable(inputNombre);
+            _driver.FindElement(inputNombre).SendKeys(nombre);
+            WaitForBeingClickable(inputpvp);
+            _driver.FindElement(inputpvp).SendKeys(pvp);
+
+            _driver.FindElement(buscar).Click();
+
+
+        }
+
+        public bool CheckListOfBocadillos(List<string[]> expectedBocadillos)
+        {
+
+            return CheckBodyTable(expectedBocadillos, tableOfBocadillosBy);
+        }
+
+        public bool CheckMessageError(string errorMessage)
+        {
+            IWebElement actualErrorShown = _driver.FindElement(errorShownBy);
+            _output.WriteLine($"actual Message shown:{actualErrorShown.Text}");
+            return actualErrorShown.Text.Contains(errorMessage);
+        }
+
+    }
+}
